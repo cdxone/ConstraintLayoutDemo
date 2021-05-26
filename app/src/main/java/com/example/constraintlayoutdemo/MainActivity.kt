77f9785bd4c0ac748620ad95d3422063cced10f8
chendexiang <937682896@qq.com>
2021-05-26 18:01:01 +0800

    ConstraintLayout的基本用法。

--- app/src/main/java/com/example/constraintlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayoutdemo/MainActivity.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import apis.amapv2.com.listviewlibrary.activity.BaseListActivty
 import apis.amapv2.com.listviewlibrary.bean.ItemObject
-import com.example.constraintlayoutdemo.activity.Test1Activity
+import com.example.constraintlayoutdemo.activity.*
 import java.util.ArrayList
 
 class MainActivity : BaseListActivty() {
 
     override fun addData(data: ArrayList<ItemObject>?) {
-        data?.add(ItemObject("", Test1Activity::class.java))
+        data?.add(ItemObject("相对位置控制", Test1Activity::class.java))
+        data?.add(ItemObject("角度定位", Test2Activity::class.java))
+        data?.add(ItemObject("边距定位", Test3Activity::class.java))
+        data?.add(ItemObject("居中和偏移", Test4Activity::class.java))
+        data?.add(ItemObject("尺寸约束", Test5Activity::class.java))
+        data?.add(ItemObject("链", Test6Activity::class.java))
     }
 
 }
---
+++ app/src/main/java/com/example/constraintlayoutdemo/activity/Test3Activity.kt
@@ -0,0 +1,12 @@
+package com.example.constraintlayoutdemo.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.constraintlayoutdemo.R
+
+class Test3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test3)
+    }
+}
